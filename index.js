00da9e017e0513714756a771179d51348a485e68
0374flop <mikola.golovach10@gmail.com>
2025-10-22 19:01:38 +0300

    Добавлен logDebuger в экспорт и изменена структура экспорта в index.js для упрощения.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 "use strict";
-const { bot, BotManager } = require('./src/bot'); // Импортируем bot и BotManager из модуля bot/index.js
+const { bot, BotManager, logDebuger } = require('./src/bot'); // Импортируем bot и BotManager из модуля bot/index.js
 const mapLoader = require('./src/map/maploader'); // Импортируем mapLoader из модуля map/maploader.js
 const Automaploader = require('./src/map/Automaploader'); // Импортируем Automaploader из модуля map/Automaploader.js
 const DebugLogger = require('./src/debug'); // Импортируем DebugLogger из модуля debug.js
 
-module.exports = { bot, mapLoader, Automaploader, BotManager, DebugLogger }; // Экспортируем bot, mapLoader, Automaploader, BotManager и DebugLogger
+/**
+ * Зделал так чтобы было меньше експорта.
+ * Тут у нас не активированый BotManager.
+ * И logDebuger который изпользуеться в BotManager, а точнее в bot.
+ */
+const botClassAndLoger = {
+    BotManager,
+    logDebuger
+}
+
+module.exports = { bot, mapLoader, Automaploader, botClassAndLoger, DebugLogger }; // Экспортируем bot, mapLoader, Automaploader, botClassAndLoger и DebugLogger
