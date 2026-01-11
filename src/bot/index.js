163beb6443492a7313893f270ccaa6700f1f9dc7
0374flop <mikola.golovach10@gmail.com>
2026-01-11 06:29:44 +0200

    выгрузил модули красиво

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,11 +1,22 @@
 "use strict";
 
 const Bot = require('./core/core');
-const Chat = require('./modules/chat');
 const DDUtils = require('./core/ddutils');
+const BaseModule = require('./core/module');
+
+const Chat = require('./modules/chat');
+const PlayerList = require('./modules/playerlist');
+const Reconnect = require('./modules/reconnect');
+
+const StandardModules = {
+    Chat,
+    PlayerList,
+    Reconnect
+}
 
 module.exports = {
     Bot,
-    Chat,
-    DDUtils
+    DDUtils,
+    BaseModule,
+    StandardModules
 }
