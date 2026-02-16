5d3b7ff8f65fe31aa6c4d5bb6b2301ae53867afd
0374flop <mikola.golovach10@gmail.com>
2026-02-16 17:01:36 +0200

    Update .gitignore and package files; add loger0374 dependency and integrate DebugLogger in tstest.ts

--- tests/tstest.ts
+++ tests/tstest.ts
@@ -1,4 +1,7 @@
 import * as ddbot from '../lib/index.js'
+import * as loger from 'loger0374';
+
+const log = new loger.DebugLogger('record', true, true, undefined, true, { logfile: { path: 'D:\\proektiki\\ddbot.js\\tests\\test.log', autocreate: true } });
 
 const bot = new ddbot.Bot();
 
@@ -9,19 +12,20 @@ const chat = new Chat(bot);
 const reconnect = new Reconnect(bot);
 
 chat.on('anychat', async (msgraw, autormsg, text, team, client_id) => {
-    console.log(autormsg ? client_id+": "+autormsg+":" : '***', text);
+    log.log(autormsg ? client_id+": "+autormsg+":" : '***', text);
     if (text == 'exit') {
         await bot.disconnect();
         process.exit();
     }
 });
 
-const [addr, port] = '26.230.124.233:8303'.split(':');
+reconnect.start();
+
+const [addr, port] = '45.141.57.22:8316'.split(':');
 
 (async () => {
-    await bot.connect(addr, parseInt(port));
+    await bot.connect(addr, parseInt(port)).catch((reason) => {});
     chat.start();
-    reconnect.start();
     console.log('Connected!');
 
     bot.on('connect', (Connection) => {
